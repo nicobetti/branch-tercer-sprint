63daf66c4effff14f21f3665e27b268c1a373dfe
MateoBrena <matubrena@gmail.com>
2023-08-23 20:38:53 -0300

    menu lateral

---
+++ conversor.js
@@ -0,0 +1,20 @@
+const monedas = [{ nombre:"USD", valor:1}, {nombre:"ARS", valor: 349.97}, {nombre:"BRL", valor: 4.87}, {nombre:"EUR", valor: 0.92}]
+
+
+for (let i = 0; i < monedas.length; i++) {
+    const element = monedas[i];
+    console.log(element)
+}
+
+
+
+/* let monto1 = document.getElementById("monto1")
+let monto2 = document.getElementById("monto2")
+let selectorUno = document.getElementById("moneda1")
+let selectorDos = document.getElementById("moneda2")
+
+function calcular () {
+    const cantidad_uno = selectorUno.value
+    const cantidad_dos = selectorDos.value
+    
+} */
